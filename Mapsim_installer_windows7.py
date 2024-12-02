6e4204a91017ace177c40f1c3567b5181a2eea62
Tarantula Trade <85387547+Mpouransari@users.noreply.github.com>
2024-12-02 16:40:36 +0330

    Update Mapsim_installer_windows7.py

--- Mapsim_installer_windows7.py
+++ Mapsim_installer_windows7.py
@@ -7,14 +7,13 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-g_t = os.getenv("GT")
-if not g_t:
-    print("GitHub GT not found.")
-    sys.exit(1)
+github_url = "git@github.com:Mpouransari/Mapsim_Device.git"  # URL in SSH format
+download_path = os.path.join(os.environ["USERPROFILE"],"dev_mapsim_","gitrepo_","mapsim_","rep",
+"h23423678123924000000_01111102299876",
+"python-3.8.10","s",
+"Mapsim_Device")
 
-github_url = "https://github.com/Mpouransari/Mapsim_Device.git"  
-download_path = os.path.expanduser("C:\\path\\to\\your\\project\\Mapsim_Device")  
-batch_file_path = os.path.expanduser("~/Desktop/run_program.bat")  
+batch_file_path = os.path.join(os.environ["USERPROFILE"], "Desktop", "run_program.bat")
 
 def install_python():
     try:
@@ -32,12 +31,22 @@ def install_python():
             print("This script only works on Windows.")
             sys.exit(1)
 
-
 def download_code():
     print(f"Downloading source code from {github_url}...")
-    auth_url = github_url.replace("https://", f"https://{github_token}@")
-    subprocess.run(["git", "clone", auth_url, download_path], check=True)
-    print(f"Code has been successfully downloaded to {download_path}.")
+
+    try:
+        subprocess.run(["git", "--version"], check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        print("Git is installed and available.")
+    except subprocess.CalledProcessError:
+        print("Git is not installed. Please install Git first.")
+        sys.exit(1)
+
+    try:
+        subprocess.run(["git", "clone", github_url, download_path], check=True)
+        print(f"Code has been successfully downloaded to {download_path}.")
+    except subprocess.CalledProcessError as e:
+        print(f"Error cloning repository: {e}")
+        sys.exit(1)
 
 def create_batch_file():
     batch_content = f"""
@@ -49,9 +58,10 @@ def create_batch_file():
         batch_file.write(batch_content)
     print(f"Batch file has been created on the Desktop at {batch_file_path}.")
 
+
 def install():
     install_python()  
-    download_code()   
+    download_code()  
     create_batch_file()  
     print("Installation completed successfully. You can now run the program by clicking on the batch file.")
 
